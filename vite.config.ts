e3dee3906319b171a479660df15a8fc17cc379f9
shzawa <ozawa.shotaro0114@gmail.com>
2024-09-10 00:15:56 +0900

    feat

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: '/read-trak/',  // github repoと同じ名前
+  server: {
+    open: true,
+  },
 })
