4000f0477a0d0fc707c4da582ae5a42a3fae801e
shzawa <ozawa.shotaro0114@gmail.com>
2024-09-23 23:08:09 +0900

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,54 @@
-import { resolve } from "path"
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      injectRegister: 'auto', //ここの記述
+      manifest: {
+        name: 'PWA Sample Apps',
+        short_name: 'PWAApps',
+        description: 'PWAサンプルアプリ',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
   },
-  base: "/read-trak/",
+  base: '/read-trak/',
   // server: {
   //   open: true // npm run dev でブラウザ自動起動
   // }
